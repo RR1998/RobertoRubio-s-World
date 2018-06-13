317c5d25d25f7f4e5f76230a726ea41204d891e3
Roberto Rubio <00259517@uca.edu.sv>
2018-06-12 22:14:19 -0600

    Creados metodos para saber la cantidad de recursos posibles para almacenar y tambien el crecimiento de almacenamiento por nivel, tambien los recursos basicos para cada raza

--- Razas/CentroMando.java
+++ Razas/CentroMando.java
@@ -3,26 +3,82 @@ package Razas;
 import java.util.ArrayList;
 
 public class CentroMando {
-    ArrayList<Integer> Almacenamiento = new ArrayList<>();
+    ArrayList<Integer> CantidadAlmacenar = new ArrayList<>();
+    ArrayList<Integer> AlmacenamientoMaximo = new ArrayList<>();
+    int Nivel;
 
+    public int getNivel() {
+        return Nivel;
+    }
 
-    public ArrayList<Integer> Inicio(String Raza){
-        switch(Raza){
-            case "Elfo":
-                return Almacenamiento = AlmacenadoInicial(100, 100,100);
-            case "Humano":
-                return Almacenamiento = AlmacenadoInicial(100,100,100);
-            case "Muertos Vivientes":
-                return Almacenamiento = AlmacenadoInicial(100, 100, 100);
-            case "Orcos":
-                return Almacenamiento = AlmacenadoInicial(100, 100, 100);
+    public void setNivel(int nivel) {
+        Nivel = nivel;
+    }
+
+    public ArrayList<Integer> getCantidadAlmacenar() {
+        return CantidadAlmacenar;
+    }
+
+    public void setCantidadAlmacenar(ArrayList<Integer> cantidadAlmacenar) {
+        CantidadAlmacenar = cantidadAlmacenar;
+    }
+    public ArrayList<Integer> getAlmacenamientoMaximo() {
+        return AlmacenamientoMaximo;
+    }
+
+    public void setAlmacenamientoMaximo(ArrayList<Integer> almacenamientoMaximo) {
+        AlmacenamientoMaximo = almacenamientoMaximo;
+    }
+    public ArrayList<Integer> AlmacenamientoM(){
+        int Nivel = getNivel();
+        int Aux1, Aux2, Aux3;
+        if (Nivel == 0){
+            setAlmacenamientoMaximo(AlmMaximo(1000,1000,1000));
+            return AlmacenamientoMaximo = getAlmacenamientoMaximo();
+        }
+        AlmacenamientoMaximo = getAlmacenamientoMaximo();
+        Aux1 = AlmacenamientoMaximo.get(0);
+        Aux2 = AlmacenamientoMaximo.get(1);
+        Aux3 = AlmacenamientoMaximo.get(2);
+        switch(Nivel){
+            case 1:
+                setAlmacenamientoMaximo(AlmMaximo((int) Math.round(Aux1+(Aux1*0.1)),(int) Math.round(Aux2+(Aux2*0.1)), (int) Math.round(Aux3+(Aux3*0.1))));
+                return AlmacenamientoMaximo = getAlmacenamientoMaximo();
+            case 2:
+                setAlmacenamientoMaximo(AlmMaximo((int) Math.round(Aux1+(Aux1*0.3)), (int) Math.round(Aux2+(Aux2*0.3)), (int) Math.round(Aux3+(Aux3*0.3))));
+                return AlmacenamientoMaximo = getAlmacenamientoMaximo();
+            case 3:
+                setAlmacenamientoMaximo(AlmMaximo((int) Math.round(Aux1+(Aux1*0.5)), (int) Math.round(Aux2+(Aux2*0.5)), (int) Math.round(Aux3+(Aux3*0.5))));
+                return AlmacenamientoMaximo = getAlmacenamientoMaximo();
         }
         return null;
     }
+
+    public ArrayList<Integer> AlmMaximo(int Recurso1, int Recurso2, int Recurso3){
+        ArrayList<Integer> Aux = new ArrayList<>();
+        Aux.add(0,Recurso1);
+        Aux.add(1, Recurso2);
+        Aux.add(2, Recurso3);
+        return Aux;
+    }
+    public ArrayList<Integer> Inicio(String Raza){
+        switch(Raza){
+            case "Elfo":
+                return CantidadAlmacenar = AlmacenadoInicial(300, 300, 300);
+            case "Humano":
+                return CantidadAlmacenar = AlmacenadoInicial(300, 300, 300);
+            case "Muertos Vivientes":
+                return CantidadAlmacenar = AlmacenadoInicial(300, 300, 300);
+            case "Orcos":
+                return CantidadAlmacenar = AlmacenadoInicial(300, 300, 300);
+        }
+        return null;
+    }
+
     public ArrayList<Integer> AlmacenadoInicial(int Recurso1, int Recurso2, int Recurso3){
-        Almacenamiento.add(0,Recurso1);
-        Almacenamiento.add(1,Recurso2);
-        Almacenamiento.add(2,Recurso3);
-        return Almacenamiento;
+        CantidadAlmacenar.add(0,Recurso1);
+        CantidadAlmacenar.add(1,Recurso2);
+        CantidadAlmacenar.add(2,Recurso3);
+        return CantidadAlmacenar;
     }
 }
