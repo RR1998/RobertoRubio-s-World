3a4a95c98b97e839158c09343afcbf74eafe0309
Roberto Rubio <00259517@uca.edu.sv>
2018-06-14 23:19:05 -0600

    Actualizacion de historia

--- Juego.java
+++ Juego.java
@@ -81,24 +81,27 @@ public class Juego {
         System.out.println("Los Elfos de la Noche\n");
         System.out.println("Bajo los espesos bosques de Ashenvale se encuentran los misteriosos Elfos de la noche \n" +
                 "Seres de forma humana que viven apartados de todos con el unico interes de proteger sus bosques.\n" +
-                "Rara vez se relacionan con otras razas ya que creen que podrian corromper sus dominios pero los tiempos han cambiado\n" +
-                "y ha llegado la hora de defender sus preciados dominios armados con el poder de la luna y arcos bendecidos por esta\n" +
-                "estan decididos a vencer a cualquiera que se les interponga\n");
+                "Rara vez se relacionan con otras razas ya que creen que podrian corromper sus dominios y linaje pero los tiempos han cambiado\n" +
+                "y ha llegado la hora de defender sus preciados territorios. Armados con el poder de la luna y arcos bendecidos por esta\n" +
+                "estan decididos a vencer a cualquiera que se les interponga todo esto en nombre de su diosa, Elune\n");
         System.out.println("Los Humanos de Lordaeron\n");
-        System.out.println("Expulsados de su tierra natal por los despiadados muertos vivientes los humanos buscan ascentarse en los valles de Kalimdor\n" +
+        System.out.println("Debilitados por constantes guerras contra sus antiguos enemigos la Orda de los orcos \n" +
+                " los humanos no pudieron defenderse de la aplastante invacion de los despiadados muertos vivientes que ls llevo asi \n" +
+                " a tener que buscar refugio en los valles del antiguo continente de Kalimdor\n" +
                 "teniendo que luchar contra el inclemente continente inexplorado donde solo unas pocas exploraciones humanas habian arribado\n" +
                 "armados con armas de fuego de los enanos y la arquitectura robusta caracteristica de los humanos estan listos para soportar a cualquier enemigo.\n");
         System.out.println("La plaga de los Muertos Vivientes\n");
         System.out.println("Invocados por el necromancer Kel'tuzad escoltado por el principe caido Arthas los muertos tienen un solo objetivo,\n" +
-                "arrasar con todo a su paso para preparar la invasion de la legion de fuego, los muertos vivientes no tienen piedad contra sus enemigos\n" +
-                "se dice que muchos de sus soldados llamados las abominaciones estan hechos de piezas de sus enemigos y que la forma de operar de su armada\n" +
-                "es sembrar el terror en sus enemigos por medio de sus armas que utilizan piezas humanas como municion o sus invocaciones de magia profana\n" +
-                "ya han conquistado todo el continente del este y solo les queda uno... Kalimdor\n");
+                "arrasar con todo a su paso para preparar la invasion de la legion de fuego, los muertos vivientes no tienen misericordia contra sus enemigos\n" +
+                " se dice que muchos de sus soldados llamados las abominaciones estan hechos de piezas de sus enemigos y que la forma de operar de su armada\n" +
+                " es sembrar el terror en sus enemigos por medio de sus armas que utilizan miembros de cadaveres como municion o sus invocaciones de magia profana\n" +
+                " ya han conquistado todo el continente del este y solo les queda uno... Kalimdor\n");
         System.out.println("La Orda de los Orcos\n");
-        System.out.println("Expulsados del continente del este por los humanos y guiados por su lidet Thrall hacia Kalimdor\n" +
-                "los orcos son feroces gerreros, despiadados en el campo de batalla y sangrientos mas que nadie se caracterizan\n" +
-                "por sus escaramuzas veloces y debastadoras y por colocar sus poderosas barricadas anti-peronal para defender sus construcciones\n" +
-                "esta civilizacion esta hecha para hacer ataques rapidos y destruir a sus enemigos de manera sopresi\n");
+        System.out.println("Expulsados del continente del este por los humanos y guiados por su lidet Thrall hacia  el viejo continente de Kalimdor\n" +
+                "los orcos, feroces gerreros, despiadados en el campo de batalla y sangrientos mas que nadie se caracterizan\n" +
+                "por sus escaramuzas veloces y devastadoras, siempre han sido los primeros en armar su maquinaria belica" +
+                "a pesar de eso a la larga sus grandes ejercitos siempre fueron aplastados por pequenos batallones humanos lo que los llevo a su derrota" +
+                "fueron tan humillados que los humanos empezaron a conciderarlos una banda de simples ladrones... pero, sera realmente asi?\n");
 
         for(int i = 0; i<2; i++) {
             int Opcion;
